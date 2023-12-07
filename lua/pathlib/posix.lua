3fcb01fe28fb86f4f1582a4e6ad59d0e93f1e616
pysan3 <pysan3@gmail.com>
2023-12-07 21:25:40 +0900

    refactor(posix): change variable name to be consistent

--- lua/pathlib/posix.lua
+++ lua/pathlib/posix.lua
@@ -1,5 +1,5 @@
 local Path = require("pathlib.base")
-local errors = require("pathlib.utils.errors")
+local err = require("pathlib.utils.errors")
 
 ---@class PathlibPosixPath : PathlibPath
 local PosixPath = {}
@@ -16,7 +16,7 @@ setmetatable(PosixPath, {
 function PosixPath:_init(...)
   Path._init(self, ...)
   if self.__windows_panic then
-    errors.multiline_error(
+    err.multiline_error(
       "RuntimeError",
       ("'%s' looks like a Windows path but you are using PosixPath."):format(self),
       [[If this is intended, use `require("pathlib.windows")` instead.]]
