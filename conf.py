723f9cd0d12a4bb8961cff4c2ac7edbfcf50a280
takuto <pysan3@gmail.com>
2024-04-26 23:18:32 +0900

    chore(main): release 2.2.2 (#60)

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@
 project = 'pathlib.nvim'
 copyright = '2023, pysan3'
 author = 'pysan3'
-release = '2.2.1' # x-release-please-version
+release = '2.2.2' # x-release-please-version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
